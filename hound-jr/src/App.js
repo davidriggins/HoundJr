a85e757d86414dcc07d09d041e0727e068608804
David Riggins <driggins@esri.com>
2023-09-12 17:54:53 -0700

    Added most of the original CSS and have select window.

--- hound-jr/src/App.js
+++ hound-jr/src/App.js
@@ -1,3 +1,9 @@
+import { useRef } from "react";
+
+// var css = function (el, n, v) {
+//     el.style.setProperty(n, v, "");
+// };
+
 function App() {
     return (
         <div className="container">
@@ -8,60 +14,12 @@ function App() {
                     placeholder="Search"
                     aria-label="Search"
                     autoComplete="off"
-                    className="btn-block"
                 />
                 <div className="button-add-on">
                     <button id="dodat" title="Search"></button>
                 </div>
             </div>
-            <div id="inb">
-                <div id="advanced">
-                    <span className="hide-adv"></span>
-
-                    <div className="field">
-                        <label className="multiselect_label" htmlFor="repos">
-                            Select Repo
-                        </label>
-                        <div className="field-input">
-                            <select
-                                id="repos"
-                                className="form-control multiselect"
-                                multiple={true}
-                                size={Math.min(16, 10)}
-                            >
-                                {}
-                            </select>
-                        </div>
-                    </div>
-
-                    <div className="ban">
-                        <em>Advanced:</em> ignore case, filter by path, stuff
-                        like that.
-                    </div>
-                </div>
-            </div>
-
-            {/* <div id="ina">
-                <input
-                    id="q"
-                    type="text"
-                    placeholder="Search by Regexp"
-                    aria-label="Search by Regexp"
-                    ref="q"
-                    autocomplete="off"
-                    onKeyDown={this.queryGotKeydown}
-                    onFocus={this.queryGotFocus}
-                />
-                <div className="button-add-on">
-                    <button
-                        id="dodat"
-                        title="Search"
-                        onClick={this.submitQuery}
-                    ></button>
-                </div>
-            </div> */}
-
-            {/* <div className="field">
+            <div className="field">
                 <label className="multiselect_label" htmlFor="repos">
                     Select Repo
                 </label>
@@ -70,13 +28,12 @@ function App() {
                         id="repos"
                         className="form-control multiselect"
                         multiple={true}
-                        size={Math.min(16, repoCount)}
-                        ref="repos"
+                        size={Math.min(16, 10)}
                     >
-                        {repoOptions}
+                        {"Hi"}
                     </select>
                 </div>
-            </div> */}
+            </div>
         </div>
     );
 }
